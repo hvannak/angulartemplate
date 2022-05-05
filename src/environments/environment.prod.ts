cf5a4bbac3987bcefb76341699a9960b18221de0
hvannak <vannak2010@gmail.com>
2022-05-05 09:43:15 +0700

    clear code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  // apiURL: 'http://192.168.100.140:8184/api', 
+  apiURL: 'http://192.168.100.140:8585/api', 
   // apiURL: 'http://192.168.100.7:8183/api', 
-  apiURL: 'http://203.189.132.22:8184/api',
+  // apiURL: 'http://203.189.132.22:8184/api',
   format: 'yyyy-MM-dd',
   locale: 'en-US'
 };
